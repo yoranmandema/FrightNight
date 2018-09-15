fccf2810e737c3eb1a051225539fdc4266dec80c
Blieper <yoranmandema@eml.cc>
2018-09-15 19:18:14 +0200

    Player movement + aim + light

---
+++ Horrogue/Assets/Scripts/Player/PlayerAim.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAim : MonoBehaviour {
+
+    public GameObject Light;
+    public GameObject Cam;
+
+    public float LightRotateSpeed;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+        SetLightRotation();
+    }
+
+    void SetLightRotation () {
+        //Get the Screen positions of the object
+        var positionOnScreen = Camera.main.WorldToViewportPoint(Light.transform.position);
+
+        //Get the Screen position of the mouse
+        var mouseOnScreen = (Vector2)Camera.main.ScreenToViewportPoint(Input.mousePosition);
+
+        //Get the angle between the points
+        var angle = AngleBetweenTwoPoints(positionOnScreen, mouseOnScreen);
+
+        var newRotation = Quaternion.Slerp(Light.transform.rotation, Quaternion.Euler(new Vector3(0f, 0f, angle + 90)),LightRotateSpeed * Time.deltaTime);
+
+        //Ta Daaa
+        Light.transform.rotation = newRotation;
+    }
+
+    float AngleBetweenTwoPoints(Vector3 a, Vector3 b) {
+        return Mathf.Atan2(a.y - b.y, a.x - b.x) * Mathf.Rad2Deg;
+    }
+}
---
+++ Horrogue/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float MaxVelocity;
+    public float Accelaration;
+
+    private Rigidbody2D rb;
+
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+	void FixedUpdate () {
+        rb.velocity += new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized * Accelaration;
+
+        rb.velocity = Vector3.ClampMagnitude(rb.velocity, MaxVelocity);
+    }
+}
