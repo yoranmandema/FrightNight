ad680a27e6cc0edaa902d083475127cd72fca483
Blieper <yoranmandema@eml.cc>
2018-09-18 00:28:07 +0200

    sound test

---
+++ Horrogue/Assets/Enemies/Test/PlayerDistanceSound.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class PlayerDistanceSound : MonoBehaviour {
+    AudioSource audioData;
+    public Transform target;
+
+    void Start() {
+        audioData = GetComponent<AudioSource>();
+        audioData.Play(0);
+    }
+
+    void Update() {
+        audioData.volume = Mathf.Clamp(Mathf.Sin(((3 - Vector3.Distance(transform.position, target.position)) / 3) * Mathf.PI / 4),0,1);
+    }
+}
