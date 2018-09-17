2432575a0abfe4e99dc0bb593ff2488bfa34d503
Blieper <yoranmandema@eml.cc>
2018-09-17 20:04:16 +0200

    CanSeePlayer state

---
+++ Horrogue/Assets/Scripts/AI/LookForPlayerState.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+class LookForPlayerState : StateMachineBehaviour {
+    [Tooltip("Determins how far the enemy can sense the player.")]
+    [SerializeField] private float _lookRadius = 10.0f;
+
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+        var player = GameObject.Find("Player");
+        var gameObject = animator.gameObject;
+
+        RaycastHit2D rayResult = Physics2D.Raycast(
+                gameObject.transform.position, 
+                player.transform.position - gameObject.transform.position, 
+                _lookRadius 
+            );
+
+        if (rayResult.collider != null) {
+            animator.SetBool("CanSeePlayer", rayResult.collider.gameObject == player);
+        } else {
+            animator.SetBool("CanSeePlayer", false);
+        }
+
+        Debug.Log(animator.GetBool("CanSeePlayer"));
+    }
+}
---
+++ Horrogue/Assets/Scripts/Rendering/LitSpriteRenderer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class LitSpriteRenderer : MonoBehaviour {
+    [SerializeField]
+    private Texture spriteTexture;
+    [SerializeField]
+    private Texture normalTexture;
+    [SerializeField]
+    private Color color = Color.white;
+
+    void OnValidate() {
+        Renderer renderer = GetComponent<Renderer>();
+
+        renderer.material.EnableKeyword("_NORMALMAP");
+
+        renderer.material.color = color;
+        renderer.material.SetTexture("_MainTex", spriteTexture);
+        renderer.material.SetTexture("_BumpMap", normalTexture);
+    }
+}
