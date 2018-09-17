5cb4c373d8e20155e4b5dd319aa9532b2aae3699
Blieper <yoranmandema@eml.cc>
2018-09-17 23:06:34 +0200

    navgrid cutter component

---
+++ Horrogue/Assets/Scripts/Navigation/NavmeshCutter.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+[ExecuteInEditMode]
+public class NavmeshCutter : MonoBehaviour {
+
+    Bounds lastBounds;
+
+	// Update is called once per frame
+	void Update () {
+        CutNavmesh();
+    }
+
+    void OnValidate() {
+        CutNavmesh();
+    }
+
+    public void CutNavmesh () {
+        if (lastBounds != null) {
+            // change some settings on the object
+            AstarPath.active.UpdateGraphs(new GraphUpdateObject(lastBounds));
+        }
+
+        var bounds = GetComponent<Collider2D>().bounds;
+        // Expand the bounds along the Z axis
+        bounds.Expand(Vector3.forward * 1000);
+        var guo = new GraphUpdateObject(bounds);
+        // change some settings on the object
+        AstarPath.active.UpdateGraphs(guo);
+
+        lastBounds = bounds;
+    }
+}
