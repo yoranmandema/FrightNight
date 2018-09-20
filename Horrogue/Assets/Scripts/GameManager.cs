b2d153d4238f66b6f88520d1cdb2ce8bbc28e6ea
Daniel <arctic-crow@web.de>
2018-09-20 09:53:24 +0200

    Some procedural basic stuff

---
+++ Horrogue/Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	#region Public Variables
+	// Reference to layout generator
+	public SimpleLayoutGenerator generator;
+
+	#endregion
+
+	#region Private Variables
+
+
+
+	#endregion
+
+	// Use this for initialization
+	void Start () {
+		if (generator == null)
+		{
+			generator = GetComponent<SimpleLayoutGenerator>();
+		}
+
+		generator.GenerateLayout();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Horrogue/Assets/Scripts/PCG/SimpleLayoutGenerator.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class SimpleLayoutGenerator : MonoBehaviour {
+
+	public enum TileType
+	{
+		Nothing,
+		Ground,
+		Wall,
+	}
+
+	#region Public Variables
+	public bool useRandomSeed;
+	public string seed = "elementary";
+	public BoundsInt generationBounds;
+	#endregion
+
+	#region Private Variables
+	private int[,] map;
+	#endregion
+
+	public void GenerateLayout()
+	{
+		InitializeRandom();
+		InitializeMap();
+
+		PlaceCorridors();
+		PlaceRooms();
+	}
+	
+	private void InitializeRandom()
+	{
+		if (useRandomSeed)
+		{
+			seed = System.DateTime.Now.Ticks.ToString();
+		}
+		Random.InitState(seed.GetHashCode());
+	}
+
+	private void InitializeMap()
+	{
+		map = new int[generationBounds.xMax, generationBounds.yMax];
+		for (int x = generationBounds.xMin; x < generationBounds.xMax; x++)
+		{
+			for (int y = generationBounds.yMin; y < generationBounds.yMax; y++)
+			{
+				map[x, y] = 0;
+			}
+		}
+	}
+
+	private void PlaceRooms()
+	{
+		throw new NotImplementedException();
+	}
+
+	private void PlaceCorridors()
+	{
+		throw new NotImplementedException();
+	}
+
+}
