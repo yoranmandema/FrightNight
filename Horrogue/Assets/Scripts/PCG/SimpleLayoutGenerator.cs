8d119361826241f10345c0d2ed2b659dc2845296
ArcticCrow <arctic-crow@web.de>
2018-09-20 17:35:30 +0200

    Main Corridor Generation, started sub corridor generation

--- Horrogue/Assets/Scripts/PCG/SimpleLayoutGenerator.cs
+++ Horrogue/Assets/Scripts/PCG/SimpleLayoutGenerator.cs
@@ -16,8 +16,14 @@ public class SimpleLayoutGenerator : MonoBehaviour {
     [Serializable]
     public struct TileSprites
     {
+        public TileSprites(TileType type)
+        {
+            this.tileType = type;
+            this.tilePrefabs = new List<GameObject>(1);
+        }
+
         public TileType tileType;
-        public List<Sprite> sprites;
+        public List<GameObject> tilePrefabs;
     }
 
 	#region Public Variables
@@ -25,11 +31,18 @@ public class SimpleLayoutGenerator : MonoBehaviour {
 	public string seed = "elementary";
 	public BoundsInt generationBounds;
     public int tileSize = 1;
-    public List<TileSprites> tileSprites;
+    public List<TileSprites> tileSprites = new List<TileSprites>()
+    {
+        new TileSprites(TileType.Air),
+        new TileSprites(TileType.Ground),
+        new TileSprites(TileType.Wall),
+    };
 	#endregion
 
 	#region Private Variables
 	private int[,] map;
+    private GameObject[,] tilemap;
+    private GameObject parent;
     #endregion
 
     private void Update()
@@ -45,11 +58,32 @@ public class SimpleLayoutGenerator : MonoBehaviour {
 		InitializeRandom();
 		InitializeMap();
 
-		PlaceCorridors();
-		PlaceRooms();
+		MapCorridors();
+		MapRooms();
+
+        PlaceTiles();
 	}
-	
-	private void InitializeRandom()
+
+    private void PlaceTiles()
+    {
+        for (int x = 0; x < generationBounds.size.x; x++)
+        {
+            for (int y = 0; y < generationBounds.size.y; y++)
+            {
+                Vector3 position = new Vector3(generationBounds.xMin + (x + 0.5f) * tileSize,
+                    generationBounds.yMin + (y + 0.5f) * tileSize);
+                Vector3 size = Vector3.one * tileSize;
+
+                GameObject tilePrefab; 
+                TileType tileType = (TileType)map[x, y];
+                tilePrefab = tileSprites[(int)tileType].tilePrefabs[0];
+
+                tilemap[x, y] = Instantiate(tilePrefab, position, Quaternion.identity, parent.transform);
+            }
+        }
+    }
+
+    private void InitializeRandom()
 	{
 		if (useRandomSeed)
 		{
@@ -60,48 +94,103 @@ public class SimpleLayoutGenerator : MonoBehaviour {
 
 	private void InitializeMap()
 	{
-		map = new int[generationBounds.size.x, generationBounds.size.y];
-		for (int x = 0; x < generationBounds.size.x; x++)
+        if (parent != null)
+        {
+            Destroy(parent);
+            parent = null;
+        }
+        parent = new GameObject("Tile Map");
+        map = new int[generationBounds.size.x, generationBounds.size.y];
+		tilemap = new GameObject[generationBounds.size.x, generationBounds.size.y];
+        for (int x = 0; x < generationBounds.size.x; x++)
 		{
 			for (int y = 0; y < generationBounds.size.y; y++)
 			{
 				map[x, y] = (int) TileType.Air;
-			}
+				tilemap[x, y] = null;
+            }
 		}
 	}
 
-    private void PlaceCorridors()
+    private void MapCorridors()
     {
         // First place a main corridor if none are placed
         // Some temporary parameters
-        int mainCorridorWidth = 7;
-        int mainCorridorHalf = 4;
+        int mainCorridorHalf = 3;
+        int numSubCorridors = 2;
 
         // Pick random spot inside given bounds and extend a corridor in one direction to its maximum length
         // Also give the generation some padding so the corridor generates with it's full width
         Vector2Int mainCorridorSpawn = new Vector2Int(Random.Range(mainCorridorHalf, generationBounds.size.x - mainCorridorHalf), 
             Random.Range(mainCorridorHalf, generationBounds.size.y - mainCorridorHalf));
 
-        // Temporary direction choosing, 0 is horizontal, 1 is vertical
-        bool corridorVert = Mathf.RoundToInt(Random.value) == 1;
+        Vector2Int[] subSpawns = new Vector2Int[numSubCorridors];
+        BoundsInt[] corridors = new BoundsInt[numSubCorridors + 1];
 
+        // Temporary direction choosing, 0 is horizontal, 1 is vertical
+        bool isVertical = Mathf.RoundToInt(Random.value) == 1;
+
+        // Determine bounds for main corridor
         int aMin = 0;
         int aMax = 0;
         int bMin = 0;
         int bMax = 0;
-        if (corridorVert) {
-            aMin = mainCorridorSpawn.x - mainCorridorHalf;
-            aMax = mainCorridorSpawn.x + mainCorridorHalf;
+        
 
-            bMin = 0 + mainCorridorHalf;
-            bMax = generationBounds.size.y - mainCorridorHalf;
-        }
-        else {
-            aMin = 0 + mainCorridorHalf;
-            aMax = generationBounds.size.x - mainCorridorHalf;
+        for (int i = 0; i < numSubCorridors + 1; i++)
+        {
+            corridors[i] = new BoundsInt();
+            if (i == 0) {
+                corridors[i].xMin = (isVertical) ? mainCorridorSpawn.x - mainCorridorHalf 
+                    : mainCorridorHalf;
+                corridors[i].yMin = (!isVertical) ? mainCorridorSpawn.y - mainCorridorHalf 
+                    : mainCorridorHalf;
+                corridors[i].xMax = (isVertical) ? mainCorridorSpawn.x + mainCorridorHalf 
+                    : generationBounds.size.x - mainCorridorHalf;
+                corridors[i].yMax = (!isVertical) ? mainCorridorSpawn.y + mainCorridorHalf 
+                    : generationBounds.size.y - mainCorridorHalf;
 
-            bMin = mainCorridorSpawn.y - mainCorridorHalf;
-            bMax = mainCorridorSpawn.y + mainCorridorHalf;
+                isVertical = !isVertical;
+            }
+            else
+            {
+                corridors[i].xMin = (isVertical) ? mainCorridorSpawn.x - mainCorridorHalf 
+                    : mainCorridorHalf;
+                corridors[i].yMin = (!isVertical) ? mainCorridorSpawn.y - mainCorridorHalf 
+                    : mainCorridorHalf;
+                corridors[i].xMax = (isVertical) ? mainCorridorSpawn.x + mainCorridorHalf 
+                    : generationBounds.size.x - mainCorridorHalf;
+                corridors[i].yMax = (!isVertical) ? mainCorridorSpawn.y + mainCorridorHalf 
+                    : generationBounds.size.y - mainCorridorHalf;
+            }
+            if (isVertical)
+            {
+                subSpawns[i] = new Vector2Int(mainCorridorSpawn.x, Random.Range(mainCorridorHalf, generationBounds.size.y - mainCorridorHalf));
+            }
+            else
+            {
+                subSpawns[i] = new Vector2Int(Random.Range(mainCorridorHalf, generationBounds.size.x - mainCorridorHalf), mainCorridorSpawn.y);
+            }
+
+
+            //if (isVertical)
+            //{
+            //    aMin = mainCorridorSpawn.x - mainCorridorHalf;
+            //    aMax = mainCorridorSpawn.x + mainCorridorHalf;
+
+            //    bMin = mainCorridorHalf;
+            //    bMax = generationBounds.size.y - mainCorridorHalf;
+            //}
+            //else
+            //{
+            //    aMin = mainCorridorHalf;
+            //    aMax = generationBounds.size.x - mainCorridorHalf;
+
+            //    bMin = mainCorridorSpawn.y - mainCorridorHalf;
+            //    bMax = mainCorridorSpawn.y + mainCorridorHalf;
+            //}
+
+
         }
 
         for (int x = aMin; x < aMax; x++)
@@ -116,11 +205,12 @@ public class SimpleLayoutGenerator : MonoBehaviour {
                 map[x, y] = (int) tile;
             }
         }
+
     }
 
-    private void PlaceRooms()
+    private void MapRooms()
 	{
-		//throw new NotImplementedException();
+		
 	}
 
 
@@ -136,33 +226,33 @@ public class SimpleLayoutGenerator : MonoBehaviour {
             Gizmos.DrawLine(new Vector3(generationBounds.xMax, generationBounds.yMax), new Vector3(generationBounds.xMin, generationBounds.yMax));
             Gizmos.DrawLine(new Vector3(generationBounds.xMax, generationBounds.yMax), new Vector3(generationBounds.xMax, generationBounds.yMin));
 
-            if (map != null)
-            {
-                for (int x = 0; x < generationBounds.size.x; x++)
-                {
-                    for (int y = 0; y < generationBounds.size.y; y++)
-                    {
-                        Vector3 position = new Vector3(generationBounds.xMin + (x + 0.5f) * tileSize,
-                            generationBounds.yMin + (y + 0.5f) * tileSize);
-                        Vector3 size = Vector3.one * tileSize;
-                        TileType tile = (TileType)map[x, y];
-                        switch (tile)
-                        {
-                            case TileType.Air:
-                                Gizmos.color = Color.grey;
-                                break;
-                            case TileType.Wall:
-                                Gizmos.color = Color.black;
-                                break;
-                            case TileType.Ground:
-                                Gizmos.color = Color.green;
-                                break;
-                            default: break;
-                        }
-                        Gizmos.DrawCube(position, size);
-                    }
-                }
-            }
+            //if (map != null)
+            //{
+            //    for (int x = 0; x < generationBounds.size.x; x++)
+            //    {
+            //        for (int y = 0; y < generationBounds.size.y; y++)
+            //        {
+            //            Vector3 position = new Vector3(generationBounds.xMin + (x + 0.5f) * tileSize,
+            //                generationBounds.yMin + (y + 0.5f) * tileSize);
+            //            Vector3 size = Vector3.one * tileSize;
+            //            TileType tile = (TileType)map[x, y];
+            //            switch (tile)
+            //            {
+            //                case TileType.Air:
+            //                    Gizmos.color = Color.grey;
+            //                    break;
+            //                case TileType.Wall:
+            //                    Gizmos.color = Color.black;
+            //                    break;
+            //                case TileType.Ground:
+            //                    Gizmos.color = Color.green;
+            //                    break;
+            //                default: break;
+            //            }
+            //            Gizmos.DrawCube(position, size);
+            //        }
+            //    }
+            //}
         }
     }
 }
