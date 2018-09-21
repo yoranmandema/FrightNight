ebf43bbba43f035db53ecdbf43d1f50015f0fd64
Blieper <yoranmandema@eml.cc>
2018-09-21 13:10:43 +0200

    LOS Light Asset

---
+++ Horrogue/Assets/2D-Light-of-Sight-Unity-master/Examples/Scripts/Follow.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Follow : MonoBehaviour {
+
+	public Transform targetTrans;
+	public bool freezeZ;
+
+	private Transform _trans;
+
+
+	// Use this for initialization
+	void Start () {
+		_trans = transform;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (targetTrans == null) return ;
+
+		if (freezeZ) {
+			Vector3 pos = targetTrans.position;
+			pos.z = _trans.position.z;
+			_trans.position = pos;
+		}
+		else {
+			_trans.position = targetTrans.position;
+		}
+	}
+}
