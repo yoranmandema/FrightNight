330487d724d43265c35cf2fe30e67d6d734d2ccf
Blieper <yoranmandema@eml.cc>
2018-09-25 19:58:00 +0200

    Improve death screen

--- Horrogue/Assets/Scripts/Attributes/PlayerHealth.cs
+++ Horrogue/Assets/Scripts/Attributes/PlayerHealth.cs
@@ -6,6 +6,8 @@ public class PlayerHealth : CharacterAttribute {
     public bool IsAlive = true;
     public GUIStyle style;
 
+    private float timeOfDeath;
+
     public override void Initialise() {
         style = new GUIStyle();
         style.fontSize = 50;
@@ -29,13 +31,37 @@ public class PlayerHealth : CharacterAttribute {
         movement.enabled = false;
 
         IsAlive = false;
+
+        timeOfDeath = Time.realtimeSinceStartup;
     }
 
     void OnGUI () {
         GUI.Label(new Rect(50, Screen.height - 100, 400, 150), "HEALTH: " + _value, style);
 
         if (!IsAlive) {
+            GUIDrawRect(new Rect(0, 0, Screen.width, Screen.height), new Color(0,0,0,(Time.realtimeSinceStartup - timeOfDeath)/3));
+
             GUI.Label(new Rect(Screen.width / 2 - 200, Screen.height / 2 - 75, 400, 150), "YOU ARE DEAD", style);
         }
+    }
+
+    private static Texture2D _staticRectTexture;
+    private static GUIStyle _staticRectStyle;
+
+    public static void GUIDrawRect(Rect position, Color color) {
+        if (_staticRectTexture == null) {
+            _staticRectTexture = new Texture2D(1, 1);
+        }
+
+        if (_staticRectStyle == null) {
+            _staticRectStyle = new GUIStyle();
+        }
+
+        _staticRectTexture.SetPixel(0, 0, color);
+        _staticRectTexture.Apply();
+
+        _staticRectStyle.normal.background = _staticRectTexture;
+
+        GUI.Box(position, GUIContent.none, _staticRectStyle);
     }
 }
