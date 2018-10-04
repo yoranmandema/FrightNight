168df8f647fd0518d4e3e9a167f6b9c57317e6e7
Blieper <yoranmandema@eml.cc>
2018-10-04 17:38:45 +0200

    Camera Controller + Light rotation smoother

---
+++ Horrogue/Assets/Scripts/Player/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public float Scale = 2f;
+    public float MaxDistance = 1f;
+    public float Smoothness = 0.5f;
+    public GameObject Player;
+
+    private Vector3 desired;
+
+	void Update () {
+        var cursorPos = Camera.main.ScreenToViewportPoint(Input.mousePosition - new Vector3(Screen.width,Screen.height,0)/2);
+        var direction = (cursorPos - transform.position).normalized;
+
+        desired = Vector3.ClampMagnitude(cursorPos, MaxDistance) * Scale + new Vector3(0,0,-10);
+
+        transform.localPosition = transform.localPosition * Smoothness + desired * (1 - Smoothness);
+    }
+}
--- Horrogue/Assets/Scripts/Player/LightAim.cs
+++ Horrogue/Assets/Scripts/Player/LightAim.cs
@@ -6,13 +6,16 @@ public class LightAim : MonoBehaviour {
 
     public LOS.LOSRadialLight Light;
     public AnimationController AnimationController;
+    public float Smoothness = 0.5f;
 
     private PlayerMovement movement;
+    private Quaternion desiredRotation;
+    private Quaternion smoothedRotation;
 
     private float AngleTo(Vector2 pos, Vector2 target) {
-        Vector2 diference = target - pos;
+        Vector2 difference = target - pos;
         float sign = (target.y < pos.y) ? -1.0f : 1.0f;
-        return Vector2.Angle(Vector2.right, diference) * sign;
+        return Vector2.Angle(Vector2.right, difference) * sign;
     }
 
     private void Start()
@@ -21,10 +24,16 @@ public class LightAim : MonoBehaviour {
     }
 
     void Update () {
-	    var cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        var direction = (cursorPos - transform.position).normalized;
+	    var cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition) - new Vector3(0,0,Camera.main.transform.position.z);
+        var direction = cursorPos - Light.transform.position;
+        direction.Normalize();
 
-        var angle = AngleTo(transform.position, cursorPos);
+        float rot_z = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        desiredRotation = Quaternion.Euler(0f, 0f, rot_z);
+
+        smoothedRotation = Quaternion.Slerp(desiredRotation, smoothedRotation, Smoothness);
+
+        var angle = smoothedRotation.eulerAngles.z;
 
         Light.faceAngle = angle;
 
